ea80b78de4cc0c3f22faac8c52f47c181ada8c6b
blastord <57312373+blastord@users.noreply.github.com>
2020-04-29 10:30:05 -0400

    Update Vue.py

--- Vue.py
+++ Vue.py
@@ -10,19 +10,29 @@ class Menu():
          print(" 5- Afficher toutes les fiches \n")
          print(" 6- Afficher toutes les fiches triées par note \n")
          
+class Fiche():
+     def CreerFiche(self) :
+         listeInfoFilm = []
+         listeInfoFilm.append(Message().AfficherMessageReponse("Quel est le nom du film : "))
+         listeInfoFilm.append(Message().AfficherMessageReponse("Quelle est la catégorie du film : "))
+         listeInfoFilm.append(Message().AfficherMessageReponse("Quelle est la date de parution du film : "))
+         listeInfoFilm.append(Message().AfficherMessageReponse("Qui est le réalisateur : "))
+         for x in range (3) :
+             listeInfoFilm.append(Message().AfficherMessageReponse("Qui sont les acteurs-ices principaux (3 max) "))
+         listeInfoFilm.append(Message().AfficherMessageReponse("Quelle note donnez vous au film (/5) : "))
+         listeInfoFilm.append(Message().AfficherMessageReponse("Commentaires : "))
+         return listeInfoFilm
 
 
 class Message():
-    def AfficherMessage(self,arg) :
-        print(arg)
-    def AfficherMessageReponse(self,arg) :
+     def AfficherMessage(self,arg) :
+         print(arg)
+     def AfficherMessageReponse(self,arg) :
          temp=input(arg)
          return temp
 
 
 Menu().AfficherMenu()
-
-NomDuFilm = Message().AfficherMessageReponse("Quelle est le nom du film : ")
-print(f'le nom est {NomDuFilm}')
+print(Fiche().CreerFiche())
 
 #BITE BITE BITE BITE
