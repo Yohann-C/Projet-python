4072a354b3b1404ed64e5f67f8af479e57a50cf1
blastord <57312373+blastord@users.noreply.github.com>
2020-04-29 09:49:15 -0400

    Initial commit

---
+++ Menu.py
@@ -0,0 +1,26 @@
+## -*- Encoding: UTF-8 -*-  
+
+class Menu():
+     def AfficherMenu(self) :
+         print(" Logiciel de critique de film \n\n")
+         print(" 1- Créer une fiche \n")
+         print(" 2- Supprimer une fiche \n")
+         print(" 3- Modifier une fiche \n")
+         print(" 4- Afficher une fiche\n")
+         print(" 5- Afficher toutes les fiches \n")
+         print(" 6- Afficher toutes les fiches triées par note \n")
+
+
+class Message():
+
+    def AfficherMessage(self,arg) :
+        print(arg)
+    def AfficherMessageReponse(self,arg) :
+         temp=input(arg)
+         return temp
+
+
+Menu().AfficherMenu()
+
+NomDuFilm = Message().AfficherMessageReponse("Quelle est le nom du film : ")
+print(f'le nom est {NomDuFilm}')
